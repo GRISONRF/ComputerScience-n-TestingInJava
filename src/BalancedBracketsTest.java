04b35f2a6bfdc200f366bd2011cfa0170997a9ff
GRISONRF <grisonrf@gmail.com>
2023-01-29 15:49:13 -0700

    Add balanced brackets JUnit test

---
+++ src/BalancedBracketsTest.java
@@ -0,0 +1,66 @@
+package src;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class BalancedBracketsTest {
+    String balancedParens = "(1)";
+    String tooManyOpenParens = "((1";
+    String tooManyOpenParens1 = "(1)(";
+    String tooManyClosedParens = "(1))";
+    String startClosedParens = ")(";
+    String emptyString = "";
+    String balancedAngleBrackets = "<<>>";
+    String balancedComboBrackets = "<({})>";
+    String mismatchedBrackets = "<({)}>";
+
+    BalancedBrackets bb = new BalancedBrackets();
+
+    @Test
+    public void testBalancedBrackets_balancedParens() {
+        assertTrue(bb.balancedBrackets("(1)"));
+        assertTrue(bb.balancedBracketsNoStack("(1)"));
+
+    }
+
+    @Test
+    public void testBalancedBrackets_tooManyOpenParens() {
+        assertFalse(bb.balancedBrackets("({1)"));
+        assertFalse(bb.balancedBracketsNoStack("({1)"));
+
+    }
+
+    @Test
+    public void testBalancedBrackets_tooManyClosedParens() {
+        assertFalse(bb.balancedBrackets("({1})>"));
+        assertFalse(bb.balancedBracketsNoStack("({1})>"));
+
+    }
+
+    @Test
+    public void testBalancedBrackets_startWithClosedParens() {
+        assertFalse(bb.balancedBrackets(")("));
+        assertFalse(bb.balancedBracketsNoStack(")("));
+
+    }
+
+    @Test
+    public void testBalancedBrackets_emptyString() {
+        assertTrue(bb.balancedBrackets(""));
+        assertTrue(bb.balancedBracketsNoStack(""));
+
+    }
+
+    @Test
+    public void testBalancedBrackets_balancedComboBrackets() {
+        assertTrue(bb.balancedBrackets("<({})>"));
+        assertTrue(bb.balancedBracketsNoStack("<({})>"));
+
+    }
+
+    @Test
+    public void testBalancedBrackets_mismatchedBrackets() {
+        assertTrue(bb.balancedBrackets("<({)}>"));
+        assertTrue(bb.balancedBracketsNoStack("<({)}>"));
+
+    }
+}
